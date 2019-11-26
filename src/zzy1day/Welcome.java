7e90f72c3596f4debe990ca095123f2289ac6306
zzy <1751953135@qq.com>
2019-11-26 14:55:25 +0800

    重写toString方法

---
+++ src/zzy1day/Welcome.java
@@ -0,0 +1,34 @@
+package zzy1day;
+
+public class Welcome {
+	public static void main(String[] args) {
+		Student s = new Student("makingBug",20,"coding");
+		System.out.println(s.toString());
+		
+	}
+}
+
+class Person {
+    String name;
+    int height;
+    public void rest(){
+        System.out.println("休息一会！");
+    }  
+}
+class Student extends Person {
+    String major; //专业
+    public void study(){
+        System.out.println("学习Java");
+    }  
+    public Student(String name,int height,String major) {
+        //天然拥有父类的属性
+        this.name = name;
+        this.height = height;
+        this.major = major;
+    }
+    
+    @Override
+    public String toString() {
+    	return "name:"+name+"\n"+"height:"+height+"\n"+"major:"+major+"\n";
+    }
+}
