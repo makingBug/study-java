dc28d9e6f710e2a8c6c3123ec4c8d32fb5ba8370
zzy <1751953135@qq.com>
2019-12-20 21:30:46 +0800

    线程状态的小部分测试

--- src/MultiThreads/AllStatus.java
+++ src/MultiThreads/AllStatus.java
@@ -2,45 +2,35 @@ package MultiThreads;
 
 import java.lang.Thread.State;
 
-public class AllStatus {
-	public static void main(String[] args) {
-		Thread t = new Thread(()->{
-			for (int i = 0;i<5;i++) {
-				try {
-					Thread.sleep(100);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				System.out.println("...");
-			}
-			
-		});
+public class AllStatus implements Runnable{
+	
+	public void run() {
+		try {
+			Thread.sleep(200);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		Thread t = new Thread(new AllStatus());
+		
 		//观察状态
 		State state = t.getState();//刚开始创建一个线程还没有启动，这时是NEW状态
-		System.out.println(state); //NEW
+		System.out.println("23行代码输出："+state); //NEW
 		
 		t.start();
-		state = t.getState();
+		
 		//RUNNABLE是运行态或就绪态，根据源码注释描述：处于RUNNABLE状态的线程正在java虚拟机中执行，但它也可能正在等待
 		//来自操作系统的其他资源，例如处理器
-		System.out.println(state);
+		System.out.println("29行代码输出："+t.getState());//RUNNABLE
 		
 		
-		
-		while(state != Thread.State.TERMINATED) { //TIMED_WAITING
-			
-			try {
-				Thread.sleep(200);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			state = t.getState();
-			int num = Thread.activeCount(); //活动的线程数
-			System.out.println(num);
-			System.out.println(state);
+		for(int i = 0;i<5;i++) {
+			System.out.println("33行代码输出："+t.getState());
+			Thread.sleep(100);
 		}
-		//TERMINATED
-		
-		
 	}
 }
