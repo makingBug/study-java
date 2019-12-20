5bb3b9e81f20f11871f9902b52f1108e994b61bd
zzy <1751953135@qq.com>
2019-12-20 14:49:42 +0800

    synchronized 实现同步

---
+++ src/MultiThreads/SynchronizedDemo.java
@@ -0,0 +1,44 @@
+package MultiThreads;
+
+/* 
+ * 并发测试数据量到达10000以上更容易观察出潜在的问题
+ * 
+ * run1,锁定代码块竟然比锁定函数更费时，不太理解
+ */
+
+public class SynchronizedDemo implements Runnable {
+    private static int count = 0;
+
+    public static void main(String[] args) {
+    	long t1 = System.currentTimeMillis();
+    	SynchronizedDemo thread = new SynchronizedDemo();
+        for (int i = 0; i < 10; i++) {
+            new Thread(thread).start();
+        }
+        while(Thread.activeCount() != 1);
+        System.out.println("result: " + count);
+        long t2 = System.currentTimeMillis();
+        System.out.println((t2-t1)+"ms");
+        
+    }
+
+    @Override
+    public void run() {
+//    	run1();
+    	run2();
+    }
+    
+    public void run1() {
+    	for (int i = 0;i < 10000;i++) {
+    		synchronized(this) {
+    			count++;
+    		}
+    	}
+    }
+    public synchronized void run2() {
+    	for(int i = 0;i<10000;i++) {
+    		count ++;
+    	}
+    }
+    
+}
